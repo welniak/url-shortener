d40ffc19b5426e740fefaf41a88e5866574f008c
welniak <maciejsurmacz@gmail.com>
2024-08-05 21:45:46 +0200

    Add url validation to the route

---
+++ app/url_validator.py
@@ -0,0 +1,19 @@
+import re
+from urllib.parse import urlparse
+
+def is_valid_url(url):
+    # Regular expression for URL validation
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc]) and re.match(regex, url) is not None
+    except ValueError:
+        return False
+    
